d9235b77b084c97748c688b4bd1e7819569be336
Chinmaya Kumar Mishra <imchinu17@gmail.com>
2025-02-01 16:42:48 +0530

    update

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { StreamProvider } from "@/context/streamContext";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -22,9 +21,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <StreamProvider>
+      
         {children}
-      </StreamProvider></body>
+     </body>
     </html>
   );
 }
